705552e874fb821c65b032f362059fff14047d3a
timflee <timflee@hotmail.com>
2024-05-24 21:48:27 +1000

    Before 2d array lookup for colour index

--- main.py
+++ main.py
@@ -1,54 +1,58 @@
-function displayLedMap () {
-    numRows = ledMap.length
-    numColumns = ledMap[0].length
-}
-radio.onReceivedNumber(function (receivedNumber) {
-	
-})
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "gridMode") {
+def displayLedMap():
+    global numRows, numColumns, j, test
+    numRows = len(ledMap)
+    numColumns = len(ledMap[0])
+    j = 0
+    test = 0
+
+def on_received_number(receivedNumber):
+    pass
+radio.on_received_number(on_received_number)
+
+def on_received_string(receivedString):
+    global rowToReceive
+    if receivedString == "gridMode":
         rowToReceive = 0
-    }
-})
-radio.onReceivedValue(function (name, value) {
-	
-})
-let numRows = 0
-let rowToReceive = 0
-let ledMap: number[] = []
-let numColumns = 0
+radio.on_received_string(on_received_string)
+
+def on_received_value(name, value):
+    pass
+radio.on_received_value(on_received_value)
+
+test = 0
+j = 0
+numRows = 0
+ledMap: List[str] = []
+rowToReceive = 0
 numColumns = 0
-ledMap = []
-radio.setGroup(1)
-let tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
+i = 0
+radio.set_group(1)
+tileDisplay = Kitronik_Zip_Tile.create_zip_tile_display(1, 1, Kitronik_Zip_Tile.UBitLocations.HIDDEN)
 tileDisplay.clear()
 tileDisplay.show()
-tileDisplay.setBrightness(20)
+tileDisplay.set_brightness(20)
+numColumns = 0
 rowToReceive = 0
-let colourOrder = [
-"r",
-"b",
-"g",
-"w",
-"0",
-"i",
-"o",
-"p",
-"v",
-"y"
-]
-let colours = [
-Kitronik_Zip_Tile.colors(ZipLedColors.Red),
-Kitronik_Zip_Tile.colors(ZipLedColors.Blue),
-Kitronik_Zip_Tile.colors(ZipLedColors.Green),
-Kitronik_Zip_Tile.colors(ZipLedColors.White),
-Kitronik_Zip_Tile.colors(ZipLedColors.Black),
-Kitronik_Zip_Tile.colors(ZipLedColors.Indigo),
-Kitronik_Zip_Tile.colors(ZipLedColors.Orange),
-Kitronik_Zip_Tile.colors(ZipLedColors.Purple),
-Kitronik_Zip_Tile.colors(ZipLedColors.Violet),
-Kitronik_Zip_Tile.colors(ZipLedColors.Yellow)
-]
-basic.forever(function () {
-	
-})
+ledMap = ["b00r0000",
+    "0b0r0000",
+    "00br0000",
+    "000i0000",
+    "wwwpbwww",
+    "000r0b00",
+    "yyyoyygy",
+    "000r000b"]
+colourOrder = ["r", "b", "g", "w", "0", "i", "o", "p", "v", "y"]
+colours = [Kitronik_Zip_Tile.colors(ZipLedColors.RED),
+    Kitronik_Zip_Tile.colors(ZipLedColors.BLUE),
+    Kitronik_Zip_Tile.colors(ZipLedColors.GREEN),
+    Kitronik_Zip_Tile.colors(ZipLedColors.WHITE),
+    Kitronik_Zip_Tile.colors(ZipLedColors.BLACK),
+    Kitronik_Zip_Tile.colors(ZipLedColors.INDIGO),
+    Kitronik_Zip_Tile.colors(ZipLedColors.ORANGE),
+    Kitronik_Zip_Tile.colors(ZipLedColors.PURPLE),
+    Kitronik_Zip_Tile.colors(ZipLedColors.VIOLET),
+    Kitronik_Zip_Tile.colors(ZipLedColors.YELLOW)]
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
 function displayLedMap () {
+    i = 0
+    j = 0
     numRows = ledMap.length
     numColumns = ledMap[0].length
+    while (i < numRows) {
+        while (j < numColumns) {
+            colourIndex = 0
+            tileDisplay.setMatrixColor(j, i, colours[0])
+            j += 1
+        }
+        j = 0
+        i += 1
+    }
+    tileDisplay.show()
 }
 radio.onReceivedNumber(function (receivedNumber) {
 	
@@ -13,51 +25,59 @@ radio.onReceivedString(function (receivedString) {
 radio.onReceivedValue(function (name, value) {
 	
 })
-let numRows = 0
-let ledMap: string[] = []
+function initVariables () {
+    numColumns = 0
+    rowToReceive = 0
+    ledMap = [
+    "b00r0000",
+    "0b0r0000",
+    "00br0000",
+    "000i0000",
+    "wwwpbwww",
+    "000r0b00",
+    "yyyoyygy",
+    "000r000b"
+    ]
+    colourOrder = [
+    "r",
+    "b",
+    "g",
+    "w",
+    "0",
+    "i",
+    "o",
+    "p",
+    "v",
+    "y"
+    ]
+    colours = [
+    Kitronik_Zip_Tile.colors(ZipLedColors.Red),
+    Kitronik_Zip_Tile.colors(ZipLedColors.Blue),
+    Kitronik_Zip_Tile.colors(ZipLedColors.Green),
+    Kitronik_Zip_Tile.colors(ZipLedColors.White),
+    Kitronik_Zip_Tile.colors(ZipLedColors.Black),
+    Kitronik_Zip_Tile.colors(ZipLedColors.Indigo),
+    Kitronik_Zip_Tile.colors(ZipLedColors.Orange),
+    Kitronik_Zip_Tile.colors(ZipLedColors.Purple),
+    Kitronik_Zip_Tile.colors(ZipLedColors.Violet),
+    Kitronik_Zip_Tile.colors(ZipLedColors.Yellow)
+    ]
+}
+let colourOrder: string[] = []
 let rowToReceive = 0
+let colours: number[] = []
+let colourIndex = 0
 let numColumns = 0
+let ledMap: string[] = []
+let numRows = 0
+let j = 0
+let i = 0
+let tileDisplay: Kitronik_Zip_Tile.ZIPTileDisplay = null
 radio.setGroup(1)
-let tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
+tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
 tileDisplay.clear()
 tileDisplay.show()
 tileDisplay.setBrightness(20)
-numColumns = 0
-rowToReceive = 0
-ledMap = [
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-""
-]
-let colourOrder = [
-"r",
-"b",
-"g",
-"w",
-"0",
-"i",
-"o",
-"p",
-"v",
-"y"
-]
-let colours = [
-Kitronik_Zip_Tile.colors(ZipLedColors.Red),
-Kitronik_Zip_Tile.colors(ZipLedColors.Blue),
-Kitronik_Zip_Tile.colors(ZipLedColors.Green),
-Kitronik_Zip_Tile.colors(ZipLedColors.White),
-Kitronik_Zip_Tile.colors(ZipLedColors.Black),
-Kitronik_Zip_Tile.colors(ZipLedColors.Indigo),
-Kitronik_Zip_Tile.colors(ZipLedColors.Orange),
-Kitronik_Zip_Tile.colors(ZipLedColors.Purple),
-Kitronik_Zip_Tile.colors(ZipLedColors.Violet),
-Kitronik_Zip_Tile.colors(ZipLedColors.Yellow)
-]
 basic.forever(function () {
 	
 })
